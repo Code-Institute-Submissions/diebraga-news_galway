4b2d573702ba8aeb544d516a9214910856fe8337
unknown <diebraga0055@gmail.com>
2020-11-03 21:30:45 +0000

    reducers success fail completed + Login page bootswatch theme

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,27 @@ import Register from './pages/Register';
 import Activate from './pages/Activate';
 import ResetPassword from './pages/ResetPassword';
 import ResetPasswordConfirm from './pages/ResetPasswordConfirm';
-import Layout from './components/Layout'
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import Layout from './components/Layout';
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/reset_password' component={ResetPassword} />
-        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-        <Route exact path='/activate/:uid/:token' component={Activate} />
-      </Switch>
-    </Layout>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/reset_password' component={ResetPassword} />
+          <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+          <Route exact path='/activate/:uid/:token' component={Activate} />
+        </Switch>
+      </Layout>
+    </Router>
+  </Provider>
 );
 
 export default App;

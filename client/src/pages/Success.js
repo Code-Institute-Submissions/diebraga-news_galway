4371e1f98b8314e29c95c0b70746b202b01647e9
unknown <diebraga0055@gmail.com>
2020-11-06 23:52:32 +0000

    success page adde after check out

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import Confetti from "react-confetti";
 
 const Success = () => {
-  return (
-    <div>
-      Success
-    </div>
-  )
-}
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    }, 100);
+  });
+
+  return (
+    <>
+    <div className='text-center align-middle' style={{ marginTop: '70px' }}>
+      <Confetti width={width} height={height} numberOfPieces={450} />
+      <h1>congrats!</h1>
+      <p style={{ fontSize: '20px' }}>Your payment has been processed successfully!</p>
+      <br/>
+      <Link className='btn btn-primary' to='shop'>Shop Again</Link>
+      <br/>
+      <br/>
+      <Link className='btn btn-primary' to='home'>Home Page</Link>
+    </div>
+    </>
+  );
+}; 
+ 
 export default Success;

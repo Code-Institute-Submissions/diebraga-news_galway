2242183e11b0bed6c1a9d6ebb95e3aeb2b7bb483
unknown <diebraga0055@gmail.com>
2020-11-13 16:11:03 +0000

    added blog app + settings + summernote + fix errors

--- server/news/urls.py
+++ server/news/urls.py
@@ -4,13 +4,12 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('authors/', include('authors.urls')),
+    path('summernote/', include('django_summernote.urls')),
+    path('api/blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]

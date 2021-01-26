b34e8c1621c2ae1b2e2e0136d07c8f20896ac50e
Diego Braga <diebraga0055@gmail.com>
2021-01-26 17:15:17 +0000

    added button delete fuctionalitie to suggestions + upgrade bootstrap 5.0

--- client/src/pages/Suggestions.js
+++ client/src/pages/Suggestions.js
@@ -8,24 +8,29 @@ import { toast } from 'react-toastify';
 // Component fetches all suggestions and list them
 
 const Blog = () => {
-  const [posts, setPosts] = useState([]);
+  const [suggestions, setSuggestions] = useState([])
 
   useEffect(() => {
-    const fetchPost = async () => {
+    getData()
+  }, [])
 
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${localStorage.getItem('access')}`,
-        'Accept': 'application/json'
-      }
-    };
+  const getData = async () => {
+
+    const URL = `${process.env.REACT_APP_API_URL}/api/suggestions/`
 
     try {
-      const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/suggestions/`, config);
-      setPosts(res.data);
-    }
-    catch (err) {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `JWT ${localStorage.getItem('access')}`,
+          'Accept': 'application/json'
+        }
+      };
+  
+      const response = await axios.get(URL, config)
+      setSuggestions(response.data)
+  
+    } catch (error) {
       toast.error('Error connection!', {
         position: "top-right",
         autoClose: 2000,
@@ -38,44 +43,63 @@ const Blog = () => {
     }
   }
 
-    fetchPost();
-  }, []);
+  const removeData = (id) => {
+    const URL = `${process.env.REACT_APP_API_URL}/api/suggestions/`
 
-  const getPosts = () => {
-    let list = [];
-    let result = [];
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `JWT ${localStorage.getItem('access')}`,
+          'Accept': 'application/json'
+        }
+      };
+  
+      axios.delete(`${URL}delete/${id}`, config).then(res => {
+        const del = suggestions.filter(suggestion => id !== suggestion.id)
+        setSuggestions(del)
+      })  
 
-  posts.map(Post => {
-    return list.push(
-      <div className="position-relative row mt-3">
-        <div className="col p-4 d-flex flex-column position-static">
-          <h3 className="mb-0">{Post.user}</h3>
-          <h5 className="d-inline-block mb-2 text-primary mt-2">{Post.topic}</h5>
-          <p className="card-text mb-auto mr-2">{Post.content}</p>
-        </div>
-        <img className='d-none d-sm-block' width="180" height="230" src={Post.thumbnail} alt='' />
-      </div>
-    );
-  });
-
-  for (let i = 0; i < list.length; i += 2) {
-    result.push(
-      <div key={i} className='row mb-2'>
-        <div className='col-md-12'>
-          {list[i]}
-        </div>
-        <div className='col-md-12'>
-          {list[i+1] ? list[i+1] : null}
-        </div>
-      </div>
-    );
+      toast.info('Item deleted!', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } catch (error) {
+      toast.error('Error deleting item!', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   }
 
-  return result;
-};
+  const getItems = () => {
+      return suggestions && suggestions.map(({ id, user, topic, content }) => {
+        return (
+          <div key={id} className="position-relative row mt-3">
+          <button type="button" class="btn-close" aria-label="Close" onClick={() => removeData(id)} />
+
+            <div className="col p-4 d-flex flex-column position-static">
+              <h3 className="mb-0">{user}</h3>
+              <h5 className="d-inline-block mb-2 text-primary mt-2">{topic}</h5>
+              <p className="card-text mb-auto mr-2">{content}</p>
+            </div>
+          </div>
+          )
+        })
+      }
 
   return (
-    <>
+      <>
       <div className="p-4 p-md-5 text-light bg-primary">
         <div className="col-md-12 px-0">
           <h1 className="text-white">Suggestioin list</h1>
@@ -86,11 +110,11 @@ const Blog = () => {
 
       <div className="container mt-3 mb-5">
         <Fade duration={1200}>
-          {getPosts()}
+          {getItems()}
         </Fade>
       </div>
     </>
-  );
-};
+  )
+}
 
 export default Blog;
--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -261,7 +261,7 @@ export const reset_password_confirm = (uid, token, new_password, re_new_password
 
 export const logout = () => dispatch => {
     dispatch({ type: LOGOUT });
-    toast.error('Error connection!', {
+    toast.info('Logout successfully!', {
       position: "top-right",
       autoClose: 2000,
       hideProgressBar: false,

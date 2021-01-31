ce13dfc92e8db44a8ded87e0a94f864034f72962
Diego Braga <diebraga0055@gmail.com>
2021-01-31 20:54:18 +0000

    added update suggestion fuctionalities test ok

--- client/src/pages/Suggestions.js
+++ client/src/pages/Suggestions.js
@@ -4,10 +4,15 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FiCornerDownRight } from 'react-icons/fi';
 import { toast, ToastContainer } from 'react-toastify';
-
+import { FiEdit } from 'react-icons/fi';
+import { Button, Accordion, Card } from 'react-bootstrap';
 // Component fetches all suggestions and list them
 
 const Suggestions = () => {
+  const [newTopic, setNewTopic] = useState('');
+  const [newUser, setNewUser] = useState('');
+  const [newContent, setNewContent] = useState('');
+
   const [suggestions, setSuggestions] = useState([])
 
   useEffect(() => {
@@ -43,7 +48,7 @@ const Suggestions = () => {
     }
   }
 
-  const removeData = (id) => {
+  const deleteSuggestion = (id) => {
     const URL = `${process.env.REACT_APP_API_URL}/api/suggestions/`
 
     try {
@@ -71,7 +76,7 @@ const Suggestions = () => {
       })  
 
     } catch (error) {
-      toast.error('You do not have permission to delete this item!', {
+      toast.error('Error removing item!', {
         position: "top-right",
         autoClose: 2000,
         hideProgressBar: false,
@@ -83,21 +88,109 @@ const Suggestions = () => {
     }
   }
 
+  const updateSuggestion = (id) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `JWT ${localStorage.getItem('access')}`,
+          'Accept': 'application/json'
+        }
+      };
+  
+      axios.put(`${process.env.REACT_APP_API_URL}/api/suggestions/update/${id}`, { 
+        topic: newTopic, 
+        id: id, 
+        user: newUser, 
+        content: 
+        newContent
+       }, config).then(
+        (response) => {
+          setSuggestions(
+            suggestions.map((val) => {
+              return val.id === id
+              ? {
+                  id: val.id,
+                  topic: newTopic,
+                  user: newUser,
+                  content: newContent,
+                }
+              : val;
+            })
+          );    
+          toast.info('Item updated!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });        
+        });
+
+      } catch (error) {
+        toast.error('Error updating item!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });  
+      }
+    };
+
+  
   const getItems = () => {
-      return suggestions && suggestions.map(({ id, user, topic }) => {
+      return suggestions && suggestions.map(({ id, user, topic, slug }) => {
         return (
           <>
-          <div key={id} className="mt-3 d-flex justify-content-between">
+          <div className="mt-3 d-flex justify-content-between" key={slug}>
             <Link to={`/suggestions/${id}`} className="mt-3">
               <div className="col p-4 d-flex flex-column position-static">
                 <h6 className="mb-0">{user}</h6>
-                <h6 className="d-inline-block mb-2 text-primary mt-2">{topic}  <FiCornerDownRight /></h6>
+                <h6 className="d-inline-block text-primary mt-2">{topic}  <FiCornerDownRight /></h6>
               </div>
             </Link>
             <div>
-              <button type="button" className="btn-close" aria-label="Close" onClick={() => removeData(id)} /><br /><br />
+              <button type="button" className="btn-close btn btn-light" aria-label="Close" onClick={() => deleteSuggestion(id)} /><br />
             </div>
           </div>
+          <Accordion>
+            <Accordion.Toggle as={Button} className='btn btn-light' variant="link" eventKey="0">
+              <FiEdit />
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body className="position-sticky">
+              <h6 className="d-inline-block text-primary mt-2">Edit <b className='text-danger'>{user}'s</b> suggestion <FiCornerDownRight /></h6>
+              <input
+                className="form-control mt-3"
+                type="email"
+                placeholder="Your email account"
+                onChange={(event) => {setNewUser(event.target.value);}}
+                required
+              />
+              <input
+                className="form-control mt-3"
+                type="text"
+                placeholder="Your topic"
+                onChange={(event) => {setNewTopic(event.target.value);}}
+                required
+              />
+              <textarea
+                className="form-control mt-3"
+                placeholder="Content"
+                onChange={(event) => {setNewContent(event.target.value);}}
+                required
+              />
+              <Button className="mt-2" variant="primary" onClick={() => {updateSuggestion(id)}}>
+                Save
+              </Button>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Accordion>
           </>
           )
         })
@@ -108,7 +201,7 @@ const Suggestions = () => {
       <div className="p-4 p-md-5 text-light bg-primary">
         <div className="col-md-12 px-0">
           <h1 className="text-white">Suggestion list</h1>
-          <p className="my-3">Suggestions made by users. <b className="text-danger">note: Only staff can delete suggestions.</b></p>
+          <p className="my-3">Suggestions made by users. <b className="text-danger">note: only staff can delete or edit suggestions.</b></p>
           <p className="mb-0"><Link to='/suggest' className="text-white">Suggest <FiCornerDownRight /></Link></p>
         </div>
       </div>

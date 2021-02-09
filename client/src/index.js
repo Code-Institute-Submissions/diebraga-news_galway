29e1ca50d86b9cae247592e6e9b4085b5d5cd51a
Diego Braga <diebraga0055@gmail.com>
2021-02-09 21:22:52 +0000

    added ingtercom chat boot integrated

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootswatch/dist/lux/bootstrap.min.css';
+import { IntercomProvider } from 'react-use-intercom';
+
+const INTERCOM_APP_ID = process.env.INTERCOM_ID;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IntercomProvider autoBoot appId={INTERCOM_APP_ID}>
+      <App />
+    </IntercomProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
